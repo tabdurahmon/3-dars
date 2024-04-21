3dc5721e278167dc118088cd26cbda81de96eb48
tabdurahmon <dilsozxonxatamova@gmail.com>
2024-04-21 23:40:35 +0500

    3-dars vazifa

---
+++ app.js
@@ -0,0 +1,129 @@
+// let son = prompt("son kiriting");
+
+// if (0 < son) {
+//   console.log("true");
+// } else {
+//   console.log("falce");
+// }
+
+// let a = prompt("son kiriting");
+
+// if (a % 2 == 0) {
+//   console.log("juft son");
+// } else {
+//   console.log("toq son");
+// }
+
+// let a = prompt("son kiriting");
+
+// if (a % 3 == 0) {
+//   console.log("juft son");
+// } else {
+//   console.log("toq son");
+// }
+
+// let a = prompt("son kiriting");
+// let b = prompt("son kiriting");
+
+// if (a > 2 && b <= 3) {
+//   console.log("true");
+// } else {
+//   console.log("falce");
+// }
+
+// let a = prompt("son kiriting");
+// let b = prompt("son kiriting");
+
+// if (a => 0 //  b < -2) {
+//   console.log("true");
+// } else {
+//   console.log("falce");
+// }
+
+// let a = prompt("son kiriting");
+// let b = prompt("son kiriting");
+// let c = prompt("son kiriting");
+
+// if (a<=b<=c) {
+//   console.log("true");
+// } else {
+//   console.log("falce");
+// }
+
+// let a = prompt("son kiriting");
+// let b = prompt("son kiriting");
+
+// if ((a && b) % 3 == 0) {
+//   console.log("true");
+// } else {
+//   console.log("falce");
+// }
+
+// let a = prompt("son kiriting");
+// let b = prompt("son kiriting");
+
+// if ((a // b) % 3 == 0) {
+//   console.log("true");
+// } else {
+//   console.log("falce");
+// }
+
+// let a = prompt("son kiriting");
+// let b = prompt("son kiriting");
+
+// if ((a && b) % 3 // (a && b) % 2 == 0) {
+//   console.log("true");
+// } else {
+//   console.log("falce");
+// }
+
+// let a = prompt("son kiriting");
+// let b = prompt("son kiriting");
+// let c = prompt("son kiriting");
+
+// if ((a//b//c) > 0 ) {
+//   console.log("true");
+// } else {
+//   console.log("falce");
+// }
+
+// let a = prompt("son kiriting");
+
+// if ((a % 10) % 2 == 0) {
+//   console.log("true");
+// } else {
+//   console.log("falce");
+// }
+
+// let a = prompt("son kiriting");
+
+// if ((a % 100) % 3 == 0) {
+//   console.log("true");
+// } else {
+//   console.log("falce");
+// }
+
+// let a = prompt("son kiriting");
+// let b = prompt("son kiriting");
+// let c = prompt("son kiriting");
+
+// if ((a=b) // (b=c) // (a=c)) {
+//   console.log("true");
+// } else {
+//   console.log("falce");
+// }
+
+// let son = prompt("son kiriting");
+// for (let i = 0; i <= son; i++) {
+//   console.log(i);
+// }
+
+// let son = prompt("son kiriting");
+// for (let i = son; i > 0; i--) {
+//   console.log(i);
+// }
+
+// let son = prompt("son kiriting");
+// for (let i = 0; i <= son; i++) {
+//   console.log(i + i);
+// }
